72d8525c2315f976f9c2eca4b9109c0d9fb63e4f
terrence-ou <outerrencedl@gmail.com>
2025-01-07 09:59:49 -0500

    project cleanup

--- app/routes/generate/system_prompts.py
+++
@@ -1,5 +0,0 @@
-words_loopup = "You are a English language expert with extensive knowledge of English dictionaries. Provide the most accurate definitions for a given word, along with 2 to 3 example sentences. If the word could be a verb, return the tenses, otherwise make it null."
-
-predict_words = "You are a English language expert with extensive knowledge of English dictionaries. If the user provides specific words, just correct the potential spelling errors and return them. If the user provides a sentence, it could be in any language, predict at most 5 possible English words. If the user provides unrelated text, set fail to ture and return fail message."
-
-predict_scene = "You are a English language expert with extensive knowledge of English dictionaries. The user could provide a word or a sentence describe a scene in any language, please predict at most 5 most relavent words use in that scene. If the user provides unrelated text, set fail to ture and return fail message."
--- core/openai.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-from openai import OpenAI
-
-load_dotenv()
-OPENAI_KEY = os.getenv("OPENAI_KEY")
-
-openai = OpenAI(api_key=OPENAI_KEY)
